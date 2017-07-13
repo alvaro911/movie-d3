8eeaf0cde18bee4c5ac3fb8746e6c0e1ca5eafcd
alvaro911 <algp232@hotmail.com>
2017-07-13 11:14:09 -0600

    display movie using the id

--- js/graph.js
+++ js/graph.js
@@ -1,10 +1,14 @@
 function renderD3(){
   const chart = document.querySelector('.chart');
+  const movieInfo = document.querySelector('.movieInfo');
+  const btns = document.querySelector('.btns')
   const width = chart.offsetWidth;
   const height = window.innerHeight;
   let page = 1;
   const key = '81b9f73460ca6ea4ee1166f130832416';
-  const smallPoster = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2'
+  const smallPoster = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2';
+  const bigPoster = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2'
+  const bgImg = 'https://image.tmdb.org/t/p/w1400_and_h450_bestv2';
 
   const svg = d3.select('.chart')
     .append('svg')
@@ -14,6 +18,7 @@ function renderD3(){
         .attr('transform', `translate(${width/2},${height/2})`);
 
   function getMovies(page){
+
     svg.selectAll('*').remove();
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=${page}`;
     d3.queue()
@@ -100,7 +105,11 @@ function renderD3(){
       .on('mouseover', tool_tip.show)
       .on('mouseout', tool_tip.hide)
       .on('click', d => {
-        console.log(d);
+        chart.style.display = 'none';
+        tool_tip.hide();
+        movieInfo.style.display = 'block';
+        btns.style.display = 'none';
+        getMovie(d);
       });
 
     const radiousScale = d3.scaleSqrt().domain([min, max]).range([30, 100]);
@@ -133,6 +142,54 @@ function renderD3(){
     }
   }
   getMovies(page);
+
+  function getMovie(d) {
+    const movie = fetch(`https://api.themoviedb.org/3/movie/${d.id}?api_key=${key}&language=en-US`)
+      .then(res => res.json())
+      .then(movieStuff => {
+        return {
+          backdropImg: bgImg + movieStuff.backdrop_path,
+          budget: movieStuff.budget,
+          genres: movieStuff.genres,
+          homepage: movieStuff.homepage,
+          title: movieStuff.title,
+          popularity: movieStuff.popularity,
+          poster: smallPoster + movieStuff.poster_path,
+          revenue: movieStuff.revenue,
+          overview: movieStuff.overview,
+        };
+      })
+      .catch(err => console.log(err));
+
+    Promise.all([movie])
+      .then(res => {
+        const movieRes = res[0];
+        const movieHTML = `
+          <div class="close-btn">X CLOSE</div>
+          <section>
+            <a href="${movieRes.homepage}" target="_blank"><div class="movie-poster">
+              <img src="${movieRes.poster}">
+            </div></a>
+            <div class="movie-detail">
+              <h1>${movieRes.title}</h1>
+              <hr>
+              <article>
+                <p>${movieRes.overview}</p>
+              </article>
+            </div>
+          </section>
+
+        `;
+        movieInfo.innerHTML = movieHTML;
+
+        document.querySelector('.close-btn').addEventListener('click', () => {
+          movieInfo.style.display = 'none';
+          chart.style.display = 'block';
+          btns.style.display = 'flex';
+        });
+      })
+  }
 }
+
 
 renderD3();
